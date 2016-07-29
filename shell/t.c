1bd9374e4f8b5b22d619444d32324e23cf9b5a75
lyt <liyutong19961231@gmail.com>
2016-07-29 19:19:09 +0000

    shanchu

--- shell/t.c
+++
@@ -1,4 +0,0 @@
-void help(void)
-{
-puts("welcome to us k-sh!\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nfunction:\n    1.history:view input history\n    2.del history:delete input history\n    3.help:open help file\n    4.cd <path>:change work path\n        *cd ~:go to your home\n    5.tab:completion input\n    6.up & down:change last or next input\n    7.you can use > < | or &\n        >:Output redirection\n        <:Input redirection\n        |:Pipe symbol\n        &:Background\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nif you have any question,you can contact me by e-mail!\ne-mail:1041984720@qq.com");
-}
